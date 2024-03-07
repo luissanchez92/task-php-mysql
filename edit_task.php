578184860ab3a9e092fa5cf6539529eddb942e03
Luis Sánchez <luisjosanchez69@gmail.com>
2024-03-06 23:35:00 -0300

    add update title-description

--- edit_task.php
+++ edit_task.php
@@ -1,16 +1,61 @@
 <?php
-include("db.php");
+    include("db.php");
 
-if(isset($_GET['id'])){
-    $id=$_GET['id'];
-    $query= "SELECT * FROM task WHERE id=$id";
-    $result=mysqli_query($conn, $query);
-    print_r($result);
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = "SELECT * FROM task WHERE id=$id";
+        $result = mysqli_query($conn, $query);
 
-    if(mysqli_num_rows($result)==1){
-        $row= mysqli_fetch_array($result);
-        print_r($row);
+        if (mysqli_num_rows($result) == 1) {
+            $row = mysqli_fetch_array($result);
+            $title = $row['title'];
+            $description = $row['description'];
+        }
     }
-}
 
+    if(isset($_POST['update'])){
+        $id=$_GET['id'];
+        $title=$_POST['title'];
+        $description=$_POST['description'];
+        $query="UPDATE task set title='$title', description='$description' where id=$id";
+        mysqli_query($conn,$query);
+
+        $_SESSION['message']='Task updated successfully';
+        $_SESSION['message_type']='warning';
+
+        header("Location: index.php");
+    }
 ?>
+
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="card card-body">
+                <form action="edit_task.php?id=<?php echo $_GET['id']; ?>" method="POST">
+                    <div class="form-group">
+                        <input
+                                type="text"
+                                name="title"
+                                value="<?php echo $title; ?>"
+                                class="form-control"
+                                placeholder="Update title"
+                        >
+                    </div>
+                    <div class="form-group mt-4">
+                        <textarea name="descripton" rows="2" class="form-control" placeholder="Update description">
+                            <?php echo $description; ?>
+                        </textarea>
+                    </div>
+
+                    <button class="btn btn-success mt-4" name="update">
+                        Update
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php") ?>
